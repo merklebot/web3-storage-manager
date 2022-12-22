fafb86b56200a8adde552f2fdb742382f9184c2d
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-12-22 16:26:10 +0600

    Remove auth and fix content router

--- web3_storage_manager/web/dependencies.py
+++ web3_storage_manager/web/dependencies.py
@@ -1,11 +1,5 @@
-from typing import Generator, Optional
+from typing import Generator
 
-from fastapi import Depends
-from fastapi.security.utils import get_authorization_scheme_param
-from sqlalchemy.orm import Session
-from starlette.requests import Request
-
-from web3_storage_manager.db.models.user import User
 from web3_storage_manager.db.session import SessionLocal
 
 
@@ -15,20 +9,3 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
-
-
-class ApiKey:
-    async def __call__(self, request: Request) -> Optional[str]:
-        authorization: str = request.headers.get("Authorization")
-        scheme, param = get_authorization_scheme_param(authorization)
-        return param
-
-
-api_key_token_scheme = ApiKey()
-
-
-async def get_current_user(
-    api_key: str = Depends(api_key_token_scheme), db: Session = Depends(get_db)
-) -> Optional[User]:
-    user = db.query(User).filter(User.api_key == api_key).first()
-    return user
--- web3_storage_manager/web/endpoints/content.py
+++ web3_storage_manager/web/endpoints/content.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 
 from web3_storage_manager.db.models.content import Content
 from web3_storage_manager.db.models.crust_order import CrustOrder
-from web3_storage_manager.db.models.user import User
 from web3_storage_manager.utils.crust import get_order_state
 from web3_storage_manager.web import dependencies
 from web3_storage_manager.web.schemas.content import ContentInDb, NewContent
@@ -14,20 +13,13 @@ router = APIRouter()
 @router.post(".add")
 async def add_content(
     new_content: NewContent,
-    user: User = Depends(dependencies.get_current_user),
     db: Session = Depends(dependencies.get_db),
 ):
-    existing_content = (
-        db.query(Content)
-        .filter(Content.cid == new_content.cid, Content.owner_id == user.id)
-        .first()
-    )
+    existing_content = db.query(Content).filter(Content.cid == new_content.cid).first()
     print(existing_content)
     if existing_content:
         return {"ok": False, "error": "Content exists"}
-    content = Content(
-        cid=new_content.cid, filesize=new_content.filesize, owner_id=user.id
-    )
+    content = Content(cid=new_content.cid, filesize=new_content.filesize)
 
     db.add(content)
     db.commit()
@@ -37,21 +29,12 @@ async def add_content(
 @router.post(".listCrustOrders")
 async def list_crust_orders(
     content_in: ContentInDb,
-    user: User = Depends(dependencies.get_current_user),
     db: Session = Depends(dependencies.get_db),
 ):
     if content_in.id:
-        content = (
-            db.query(Content)
-            .filter(Content.id == content_in.id, Content.owner_id == user.id)
-            .first()
-        )
+        content = db.query(Content).filter(Content.id == content_in.id).first()
     else:
-        content = (
-            db.query(Content)
-            .filter(Content.id == content_in.cid, Content.owner_id == user.id)
-            .first()
-        )
+        content = db.query(Content).filter(Content.id == content_in.cid).first()
 
     if not content:
         return {"ok": False, "error": "Content not found"}
@@ -65,21 +48,12 @@ async def list_crust_orders(
 @router.post(".listCrustReplicas")
 async def list_crust_replicas(
     content_in: ContentInDb,
-    user: User = Depends(dependencies.get_current_user),
     db: Session = Depends(dependencies.get_db),
 ):
     if content_in.id:
-        content = (
-            db.query(Content)
-            .filter(Content.id == content_in.id, Content.owner_id == user.id)
-            .first()
-        )
+        content = db.query(Content).filter(Content.id == content_in.id).first()
     else:
-        content = (
-            db.query(Content)
-            .filter(Content.id == content_in.cid, Content.owner_id == user.id)
-            .first()
-        )
+        content = db.query(Content).filter(Content.id == content_in.cid).first()
 
     if not content:
         return {"ok": False, "error": "Content not found"}
