74e515e7a5586ea074069334cd10d4d23a5bdbbe
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-12-22 16:23:36 +0600

    Remove users router

--- web3_storage_manager/web/api.py
+++ web3_storage_manager/web/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from web3_storage_manager.web.endpoints import content, users
+from web3_storage_manager.web.endpoints import content
 
 api_router = APIRouter()
-api_router.include_router(users.router, prefix="/users")
 api_router.include_router(content.router, prefix="/content")
