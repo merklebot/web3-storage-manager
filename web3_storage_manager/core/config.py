14eeae6a129bd0a54dd624d158046281e05834fe
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-12-22 13:39:27 +0600

    Create database uri from separated env variables

--- web3_storage_manager/core/config.py
+++ web3_storage_manager/core/config.py
@@ -1,8 +1,6 @@
-from pydantic import BaseSettings, Field
+from typing import Any
 
-
-class DBSettings(BaseSettings):
-    DATABASE_URL: str = Field(..., env="DATABASE_URL")
+from pydantic import BaseSettings, Field, PostgresDsn, validator
 
 
 class WalletSettings(BaseSettings):
@@ -11,7 +9,24 @@ class WalletSettings(BaseSettings):
 
 
 class Settings(BaseSettings):
-    DB: DBSettings = DBSettings()
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: str | None, values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
     WALLET_SETTINGS: WalletSettings = WalletSettings()
 
 
