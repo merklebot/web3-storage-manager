2abb130bf34a3a5a1c26c9f9961c512e4bd9d2ba
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-12-22 14:49:35 +0600

    Fix database uri pssing to alembic env file

--- web3_storage_manager/db/migrations/env.py
+++ web3_storage_manager/db/migrations/env.py
@@ -11,7 +11,7 @@ from web3_storage_manager.db.base import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.DB.DATABASE_URL)
+config.set_main_option('sqlalchemy.url', settings.SQLALCHEMY_DATABASE_URI)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
